22adc271195d46598d57c07c70c709375f18f692
Cristhian Agamez <rcagamez@uninorte.edu.co>
2023-05-14 00:55:50 -0500

    Players and online update

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Mono.Cecil.Cil;
 
 public class PlayerController : NetworkBehaviour
 {
@@ -10,11 +11,19 @@ public class PlayerController : NetworkBehaviour
     float moveH;
     [SerializeField] private float velocity;
     [SerializeField] private float jumpForce;
-    bool canJump = true;
+    
     private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1);
+    private Animator anim; 
+    bool _facingRight = true;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask layerMask;
+    private bool _isGrounded;
     void Start()
     {
         r2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     
@@ -28,21 +37,38 @@ public class PlayerController : NetworkBehaviour
         if (!IsOwner) return;
         moveH = Input.GetAxis("Horizontal");
         r2d.velocity = new Vector2(moveH*velocity, r2d.velocity.y);
-        if (Input.GetKey(KeyCode.Space) && canJump)
+        if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            Debug.Log("entro");
-            r2d.AddForce(Vector2.up * jumpForce);
-            canJump = false;
+            
+            r2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            
         }
         
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-
-        if (collision.tag == "Ground")
+        if (moveH > 0 && !_facingRight )
         {
-            canJump = true;
+            flip();
+        }else if (moveH < 0 && _facingRight) {
+            flip();
         }
+
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, layerMask);
+    }
+    private void LateUpdate()
+    {
+        if(moveH != 0) {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+
     }
     
+    void flip() {
+        _facingRight = !_facingRight;
+        float localScaleX = transform.localScale.x;
+        localScaleX = localScaleX * -1f;
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
 }
